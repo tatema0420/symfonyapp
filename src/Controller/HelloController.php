a171f245c1fdc7f5c3f433b3cdd55d44f6db86c8
立松開 <tatematsukai@gmail.com>
2021-02-15 01:10:27 +0900

    2-2 + gitignore

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 
-class HelloController extends AbstractController
+class HelloController 
 {
     public function index()
     {
