bf5cd82953aeef307b31ee32419e4a12f1f1ca17
立松開 <tatematsukai@gmail.com>
2021-02-15 01:14:37 +0900

    2-3

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController 
 {
+    /**
+     * @Route("/hello", name="hello")
+     */
     public function index()
     {
         return new Response('Hello Symfony!');
