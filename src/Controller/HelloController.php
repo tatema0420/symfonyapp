db66e445f7841f5a81a13ad9ee82ef5109b7e595
立松開 <tatematsukai@gmail.com>
2021-02-15 23:00:13 +0900

    2-11

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,8 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
 
 class HelloController extends AbstractController
 {
@@ -15,10 +19,19 @@ class HelloController extends AbstractController
      */
     public function index(Request $request)
     {
+        $encoders = array(new XmlEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+
         $data = array(
-            'name' => array('first' => 'Taro', 'second' => 'Yamada'),
-            'age'=>36, 'mail'=>'taro@yamada.kun'
+            'name' => array('first' => 'Hanako', 'second' => 'Tanaka'),
+            'age'=>36, 'mail'=>'hanako@flower.san'
         );
-        return new JsonResponse($data);
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'xml');
+        $result = $serializer->serialize($data, 'xml');
+        $response->setContent($result);
+        return $response;
     }
 }
