0640668b129eec19b0c9fce78d4a4b2f8962fb17
立松開 <tatematsukai@gmail.com>
2021-02-15 22:28:42 +0900

    2-8

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,16 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
     /**
-     * @Route("/hello/{name}/{pass}", name="hello")
+     * @Route("/hello", name="hello")
      */
-    public function index($name, $pass)
+    public function index(Request $request)
     {
+        $name = $request->get('name');
+        $pass = $request->get('pass');
         $result = '<html><body><ol>';
         $result .= '<h1>Subscribed Services</h1>';
         $result .= '<p>name:' . $name . '</p>';
