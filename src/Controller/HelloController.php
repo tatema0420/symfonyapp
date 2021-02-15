57335bbe9dd287dcd4c324b4c0f7f590bbebd26c
立松開 <tatematsukai@gmail.com>
2021-02-15 22:47:09 +0900

    2-10

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
@@ -15,24 +15,10 @@ class HelloController extends AbstractController
      */
     public function index(Request $request)
     {
-        $result = '<html><body><ol>';
-        $result .= '<h1>Parameter</h1>';
-        $result .= '<p>This is index page</p>';
-        $result .= '</body></html>';
-        return new Response($result);
+        $data = array(
+            'name' => array('first' => 'Taro', 'second' => 'Yamada'),
+            'age'=>36, 'mail'=>'taro@yamada.kun'
+        );
+        return new JsonResponse($data);
     }
-
-    /**
-     * @Route("/other/{domain}", name="other")
-     */
-    public function other(Request $request, $domain = '')
-    {
-        if ($domain == "") {
-            return $this->redirect('/hello');
-        } else {
-            return new RedirectResponse("http://{$domain}.com");
-        }
-    }
-
-
 }
